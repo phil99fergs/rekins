56c0709f87a7b9ec43761893fdbfe5f8cb67fdd3
phil99fergs <37699111+phil99fergs@users.noreply.github.com>
2021-09-22 11:09:15 +0300

    Create rekins.py

---
+++ rekins.py
@@ -0,0 +1,9 @@
+class Rekins:
+
+    def __init__(self,name,tekst,size,height,width,material):
+        self.name = name
+        self.tekst = tekst
+        self.size = size
+        self.height = height
+        self.width = width
+        self.material = material
